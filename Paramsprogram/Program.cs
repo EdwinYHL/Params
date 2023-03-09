6999771e21c2952ff3224d5f99d090c11596f593
EdwinYHL <126517767+EdwinYHL@users.noreply.github.com>
2023-03-09 20:56:09 +0000

    new file:   .vscode/launch.json 	new file:   .vscode/tasks.json 	new file:   Paramsprogram/Params.csproj 	new file:   Paramsprogram/Program.cs

---
+++ Paramsprogram/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+private static void increment(ref int x)
+{
+    x++;
+}
+private static void plus(int a, int b, out int c)
+{
+    c= a + b;
+}
+    private static void Main(string[] args)
+    {
+        int x = 10;
+        increment(ref x);
+        int y = 10;
+        int result;
+        plus(x,y,out result);
+        Console.WriteLine(x);
+        Console.WriteLine(result);
+    }
+}
